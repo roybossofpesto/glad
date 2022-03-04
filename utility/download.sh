f6a12791d0e541ef25d04170d424bf2f2181a3cf
roybossofpesto <andreduracell@gmail.com>
2022-03-04 02:46:18 +0100

    no check

--- utility/download.sh
+++ utility/download.sh
@@ -6,16 +6,16 @@ TARGET=${TARGET:="."}
 
 
 rm -f egl.xml
-wget -O "${TARGET}/egl.xml" https://raw.githubusercontent.com/KhronosGroup/EGL-Registry/master/api/egl.xml
+wget --no-check-certificate -O "${TARGET}/egl.xml" https://raw.githubusercontent.com/KhronosGroup/EGL-Registry/master/api/egl.xml
 
 rm -f gl.xml
-wget -O "${TARGET}/gl.xml" https://raw.githubusercontent.com/KhronosGroup/OpenGL-Registry/master/xml/gl.xml
+wget --no-check-certificate -O "${TARGET}/gl.xml" https://raw.githubusercontent.com/KhronosGroup/OpenGL-Registry/master/xml/gl.xml
 
 rm -f glx.xml
-wget -O "${TARGET}/glx.xml" https://raw.githubusercontent.com/KhronosGroup/OpenGL-Registry/master/xml/glx.xml
+wget --no-check-certificate -O "${TARGET}/glx.xml" https://raw.githubusercontent.com/KhronosGroup/OpenGL-Registry/master/xml/glx.xml
 
 rm -f wgl.xml
-wget -O "${TARGET}/wgl.xml" https://raw.githubusercontent.com/KhronosGroup/OpenGL-Registry/master/xml/wgl.xml
+wget --no-check-certificate -O "${TARGET}/wgl.xml" https://raw.githubusercontent.com/KhronosGroup/OpenGL-Registry/master/xml/wgl.xml
 
 rm -f khrplatform.h
-wget -O "${TARGET}/khrplatform.h" https://raw.githubusercontent.com/KhronosGroup/EGL-Registry/master/api/KHR/khrplatform.h
+wget --no-check-certificate -O "${TARGET}/khrplatform.h" https://raw.githubusercontent.com/KhronosGroup/EGL-Registry/master/api/KHR/khrplatform.h
